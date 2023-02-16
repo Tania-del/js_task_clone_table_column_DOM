bc97ecc9101c2a63aeabe64114c145f77d1289e8
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-02-16 22:16:43 +0200

    cloned table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const secondColumnValue = table.rows[i].cells[1].innerHTML;
+
+  const newCell = document.createElement('td');
+
+  newCell.innerHTML = secondColumnValue;
+
+  table.rows[i].insertCell(4).before(newCell);
+}
